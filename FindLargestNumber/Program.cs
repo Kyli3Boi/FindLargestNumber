06ab2c53d15c90e61be82fc394b2faab2f42eca1
Kyli3Boi <awe.kyle@gmail.com>
2021-07-22 10:17:27 +0200

    Add project files.

---
+++ FindLargestNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of nubers seperated by a \",\"");
+            Console.WriteLine("EG. 1,2,3,4,5");
+            
+            string userNumbers = Console.ReadLine();
+            string[] userNumbersSplit = userNumbers.Split(',');
+            int largestNumber = 0;
+
+            for (int i = 0; i < userNumbersSplit.Length; i++)
+            {
+                if (Convert.ToInt32(userNumbersSplit[i]) > largestNumber)
+                {
+                    largestNumber = Convert.ToInt32(userNumbersSplit[i]);
+                }
+            }
+            
+            Console.WriteLine("The largest number is: {0}", largestNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
